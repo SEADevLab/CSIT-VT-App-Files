3eb159e0f0d24349f09322212105360d1652b77e
ianliew03 <ian.liew.wen@gmail.com>
2022-10-03 14:02:17 +0800

    SEADEVLAB-13 Fetch Data And Test Upload

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { MalwareFilesContextProvider } from "./context/MalwareFileContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MalwareFilesContextProvider>
+      <App />
+    </MalwareFilesContextProvider>
   </React.StrictMode>
 );
-
